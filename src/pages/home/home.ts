aed5a55a37b9c2025b47861da64128f549d79ef3
Sael <sael1605@gmail.com>
2017-10-12 17:50:18 -0700

    alert ramiro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
 
+  doAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Que onda',
+      subTitle: 'opinshi ramiro',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
+
